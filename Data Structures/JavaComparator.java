c24b82d76d8aa052871bb40e71272d54ce8ee57b
Maria Dekova <msDekova@Marias-MacBook-Air.local>
2020-06-06 15:07:02 +0300

    Add solution

---
+++ Data Structures/JavaComparator.java
@@ -0,0 +1,50 @@
+//Given an array of n Player objects, write a comparator that sorts them in order of decreasing score;
+// if 2 or more players have the same score, sort those players alphabetically by name.
+
+import java.util.*;
+
+// Write your Checker class here
+class Checker implements Comparator{
+    @Override
+    public int compare(Object o1, Object o2){
+        Player player1 = (Player)o1;
+        Player player2 = (Player)o2;
+
+        if(player1.score == player2.score) {
+            return player1.name.compareTo(player2.name);
+        }
+        else if(player1.score < player2.score) return 1;
+        else return -1;
+    }
+}
+
+class Player{
+    String name;
+    int score;
+
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+public class JavaComparator {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
