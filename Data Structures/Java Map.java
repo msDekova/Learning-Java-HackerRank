292f54d6d8c41476b76fc3ce64a98ab8dfca4f7e
Maria Dekova <msDekova@Marias-MacBook-Air.local>
2020-06-06 13:36:04 +0300

    Add solution

---
+++ Data Structures/Java Map.java
@@ -0,0 +1,29 @@
+// You are given a phone book that consists of people's names and their phone number. After that you will be given
+// some person's name as query. For each query, print the phone number of that person.
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numOfEntries = Integer.parseInt(scanner.nextLine());
+        Map<String,Long> phoneBook = new HashMap<>();
+        while(numOfEntries-->0){
+            String name = scanner.nextLine();
+            long phoneNumber = Long.parseLong(scanner.nextLine());
+            phoneBook.put(name,phoneNumber);
+        }
+
+        while(scanner.hasNext()){
+            String query = scanner.nextLine();
+            if(phoneBook.containsKey(query)){
+                System.out.println(query + "=" + phoneBook.get(query));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+    }
+}
